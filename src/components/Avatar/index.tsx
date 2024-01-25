5d59a09091fc624546c0791b86b5be1c8cf7e03a
hamjeth <50629287+Hamjeth68@users.noreply.github.com>
2024-01-26 01:56:44 +0530

    Initialize staging

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Image } from 'react-native';
+
+interface AvatarProps {
+  imageUrl: string;
+  width?: number;
+    height?: number;
+    borderRadius?: number;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ imageUrl, width, height, borderRadius }) => {
+  return <Image source={{ uri: imageUrl}} style={{ width: width, height: height, borderRadius: borderRadius }} />;
+};
+
+export default Avatar;
